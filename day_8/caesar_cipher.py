19606f48c4c40d95405966e4ff312a47e7e0b448
[alpathyb] <[fjralfath11@gmail.com]>
2024-03-03 21:04:40 +0700

    add revision for tackle the possible bugs and iterate the program

--- day_8/caesar_cipher.py
+++ day_8/caesar_cipher.py
@@ -2,29 +2,38 @@ import alphabet
 import logo
 
 print(logo.ascii_logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Input your message here : ").lower()
-shift = int(input("Input your shift here: "))
+isContinue = True
+
+while isContinue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Input your message here : ").lower()
+    shift = int(input("Input your shift here: "))
 
 
-def caesar(direct=direction, txt=text, sft=shift):
-    if direct == "encode" or direct == "decode":
-        if isinstance(sft, int):
-            final_text = ""
-            if direct == "decode":
-                sft *= -1
-            for letter in txt:
-                if letter.isalpha():
-                    first_position = alphabet.list_alphabet.index(letter)
-                    position = (first_position + sft) % len(alphabet.list_alphabet)
-                    final_text += alphabet.list_alphabet[position]
-                else:
-                    final_text += letter
-            print(f"The {direct}d text is {final_text}")
+    def caesar(direct=direction, txt=text, sft=shift):
+        if direct == "encode" or direct == "decode":
+            if isinstance(sft, int):
+                final_text = ""
+                if direct == "decode":
+                    sft *= -1
+                for letter in txt:
+                    if letter.isalpha():
+                        first_position = alphabet.list_alphabet.index(letter)
+                        position = (first_position + sft) % len(alphabet.list_alphabet)
+                        final_text += alphabet.list_alphabet[position]
+                    else:
+                        final_text += letter
+                print(f"The {direct}d text is {final_text}")
+            else:
+                print("You input the wrong shift")
         else:
-            print("You input the wrong shift")
+            print(f"You input the wrong direction")
+
+
+    caesar(direction, text, shift)  # call the caesar cipher function
+    ask_continue = input("You want to continue the program? type yes/no ").lower()
+    if ask_continue == "yes":
+        isContinue = True
     else:
-        print(f"You input the wrong direction")
-
-
-caesar(direction, text, shift)
+        print("You finish the cipher program")
+        isContinue = False
