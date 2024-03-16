000e57c8825ffab4c0cb70a0de9fbb5b3d910bc4
alpathyb <fjralfath11@gmail.com>
2024-03-16 12:32:50 +0700

    add day 10

---
+++ Day_10/function_output.py
@@ -0,0 +1,14 @@
+def add(num1, num2):
+    return num1 + num2
+
+
+print(add(1, 2))
+
+
+def format_name(f_name, l_name):
+    firstname = f_name.title()
+    lastname = l_name.title()
+    return f"{firstname} {lastname}"
+
+
+print(format_name("fajar", "alfath"))
